487bc6fc88ef50fc35054bdf0b692cad81186efd
Martin Borho <martin@borho.net>
2017-10-06 17:58:47 +0200

    Replaced hashlib call in dynamodb controlfile client with get_hash method from components.

--- livebridge/controldata/dynamo.py
+++ livebridge/controldata/dynamo.py
@@ -13,10 +13,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import hashlib
 import logging
 from livebridge.controldata.base import BaseControl
-from livebridge.components import get_db_client
+from livebridge.components import get_db_client, get_hash
 
 logger = logging.getLogger(__name__)
 
@@ -33,9 +32,6 @@ class DynamoControl(BaseControl):
         self._db_client = get_db_client()
         return self._db_client
 
-    def _get_checksum(self, data):
-        return hashlib.md5(str(data).encode("utf-8")).hexdigest()
-
     async def  _load_control_data(self):
         db_client = await self.db_client
         return await db_client.get_control()
@@ -44,7 +40,7 @@ class DynamoControl(BaseControl):
         try:
             control_data = await self._load_control_data()
             if control_data and self._checksum \
-                and self._checksum != self._get_checksum(control_data):
+                and self._checksum != get_hash(control_data):
                 return True
         except Exception as exc:
             logger.error("Error checking dynamo control data change with: {}".format(exc))
@@ -52,7 +48,7 @@ class DynamoControl(BaseControl):
 
     async def load(self, path):
         control_data = await self._load_control_data()
-        self._checksum = self._get_checksum(control_data)
+        self._checksum = get_hash(control_data)
         return control_data
 
     async def save(self, path, data):
